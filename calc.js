1aa1aa36e79c4016bbc1ccb6e11611ff93a05acc
Maria Jose Alvarez <mj.alvarez.zurita@gmail.com>
2023-08-24 11:34:38 -0400

    Calculo 2

--- calc.js
+++ calc.js
@@ -11,14 +11,29 @@ var n1,n2, suma;
 
 seguir= prompt("Bienvenido a la calculadora, ¿quiere seguir? s/n");
 if (seguir == "s") {
-    nombre = prompt("Ingrese nombre: ");
-    n1 = parseInt(prompt("Ingrese número 1: "));
-    n2 = parseFloat(prompt("Ingrese número 2: "));
+    opcion= parseInt("1.-Suma, 2.-Resta, 3.-Multiplicacion, 4.-Division, ¿que decea hacer?");
+    if (opcion ==1){
+        console.log("Esto es suma")
+        n1 = parseInt(prompt("Ingrese número 1: "));
+        n2 = parseFloat(prompt("Ingrese número 2: "));
+    
+        suma = n1+n2;
+        console.log(suma);
+    }
+    
+    else if (opcion ==2)
+    {
+        console.log("esto es resta")
+    }
+    else if (opcion ==3)
+    {
+        console.log("esto es Multiplicacion")
+    }
+    else if (opcion ==4)
+    {
+        console.log("esto es Division")
+    }
 
-    suma = n1+n2;
-    console.log(suma);
-    suma= "hola mundo";
-    console.log(suma);
 }
 else
     console.log("chau");
